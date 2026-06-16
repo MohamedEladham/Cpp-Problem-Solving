85220e639814e0f6a549ce9415599b1da36c2104
Mohamed Eladham <mohamed.eladham97@gmail.com>
2026-06-16 10:22:57 +0200

    Solve sales commission calculation using conditional percentage tiers

---
+++ 001_Problem_Solving_Level1/034_Calculate_Sales_Commission.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+float ReadTotalSales()
+{
+	float TotalSales;
+
+	cout << "\n> Please Enter Total Sales: ";
+	cin >> TotalSales;
+
+	return TotalSales;
+}
+
+float GetCommissionPercentage(float TotalSales)
+{
+	if (TotalSales >= 1000000)
+		return 0.01;
+
+	else if (TotalSales >= 500000)
+		return 0.02;
+
+	else if (TotalSales >= 100000)
+		return 0.03;
+
+	else if (TotalSales >= 50000)
+		return 0.05;
+
+	else
+		return 0.00;
+}
+
+float CalculateTotalCommission(float TotalSales)
+{
+	return GetCommissionPercentage(TotalSales) * TotalSales;
+}
+
+int main()
+{
+	float TotalSales = ReadTotalSales();
+
+	cout << "\n> Commission Percentage = " << GetCommissionPercentage(TotalSales) << endl;
+	cout << "\n> Total Commission = " << CalculateTotalCommission(TotalSales) << endl;
+
+	return 0;
+}
