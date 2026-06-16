366f4148a5a27853c4b6d858ffb05d6d72981da1
Mohamed Eladham <mohamed.eladham97@gmail.com>
2026-06-16 08:43:03 +0200

    Add printName function implementation

---
+++ 001_Problem_Solving_Level1/001_Print_User_Name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void PrintName(string Name)
+{
+	cout << "\n> Your Name Is " << Name << endl;
+}
+
+int main()
+{
+	PrintName("Mohamed");
+	return 0;
+}
