f81e9cb0d54aac18be0178724a310f2cc22cfe27
Mohamed Eladham <mohamed.eladham97@gmail.com>
2026-06-19 07:54:02 +0200

    Solve array summation problem using linear traversal

---
+++ 002_Problem_Solving_Level2/026_Sum_All_Elements_In_Array.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+	int Number;
+
+	do
+	{
+		cout << Message;
+		cin >> Number;
+
+	} while (Number <= 0);
+
+	return Number;
+}
+
+int RandomNumber(int From, int To)
+{
+	return rand() % (To - From + 1) + From;
+}
+
+void FillArrayWithRandomNumbers(int Array[], int ArraySize)
+{
+	for (int i = 0; i < ArraySize; i++)
+	{
+		Array[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int Array[], int ArraySize)
+{
+	for (int i = 0; i < ArraySize; i++)
+	{
+		cout << Array[i] << " ";
+	}
+
+	cout << "\n";
+}
+
+int GetArraySum(int Array[], int ArraySize)
+{
+	int Sum = 0;
+
+	for (int i = 0; i < ArraySize; i++)
+	{
+		Sum += Array[i];
+	}
+
+	return Sum;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int Array[100], ArraySize;
+
+	ArraySize = ReadPositiveNumber("\n> Enter Number Of Elements: ");
+
+	FillArrayWithRandomNumbers(Array, ArraySize);
+
+	cout << "\n> Array Elements: ";
+	PrintArray(Array, ArraySize);
+
+	cout << "\n> Sum Of All Elements = "
+		<< GetArraySum(Array, ArraySize)
+		<< endl;
+
+	return 0;
+}
